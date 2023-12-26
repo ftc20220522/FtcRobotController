65cd214379a3968518b7bd838cef0532d0a6d1f7
Syntech Robotics <110001450+ftc20220522@users.noreply.github.com>
2023-12-26 14:10:28 -0600

    Create motorTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+import java.util.concurrent.TimeUnit;
+
+@TeleOp(group = "ZTest")
+public class motorTest extends LinearOpMode{
+    public void runOpMode() throws InterruptedException {
+        DcMotor motor = hardwareMap.dcMotor.get("motor1");
+        waitForStart();
+        if (isStopRequested()) return;
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                motor.setPower(0.1);
+                TimeUnit.MILLISECONDS.sleep(350);
+            } else if (gamepad1.b) {
+                motor.setPower(0);
+                TimeUnit.MILLISECONDS.sleep(350);
+            }
+        }
+    }
+}
