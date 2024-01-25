9fc5405d01769d7f347655f77717da7e4e85ee71
Syntech Robotics <110001450+ftc20220522@users.noreply.github.com>
2024-01-24 20:31:38 -0600

    Added the roadrunner values from before

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTournamentOB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTournamentOB.java
@@ -13,21 +13,46 @@ import org.firstinspires.ftc.robotcore.internal.system.Deadline;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-@Autonomous(name="AutoMeet3")
-public class AutoMeet3 extends LinearOpMode {
+@Autonomous(name="AutoTournamentOB")
+public class AutoTournamentOB extends LinearOpMode {
     private final int READ_PERIOD = 2;
-
     private HuskyLens huskyLens;
-
-
     String mode = "TAG";
-
     String pos;
-
     int location = 0;
+    double DESIRED_DISTANCE = 12.0; //  this is how close the camera should get to the target (inches)
 
+    //  Set the GAIN constants to control the relationship between the measured position error, and how much power is
+    //  applied to the drive motors to correct the error.
+    //  Drive = Error * Gain    Make these values smaller for smoother control, or larger for a more aggressive response.
+    double SPEED_GAIN  =  0.02  ;   //  Forward Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
+    double STRAFE_GAIN =  0.015 ;   //  Strafe Speed Control "Gain".  eg: Ramp up to 25% power at a 25 degree Yaw error.   (0.25 / 25.0)
+    double TURN_GAIN   =  0.01  ;   //  Turn Control "Gain".  eg: Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
+
+    double MAX_AUTO_SPEED = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+    double MAX_AUTO_STRAFE= 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+    double MAX_AUTO_TURN  = 0.3;   //  Clip the turn speed to this max value (adjust for your robot)
+
+    DcMotor leftFrontDrive   = null;  //  Used to control the left front drive wheel
+    DcMotor rightFrontDrive  = null;  //  Used to control the right front drive wheel
+    DcMotor leftBackDrive    = null;  //  Used to control the left back drive wheel
+    DcMotor rightBackDrive   = null;  //  Used to control the right back drive wheel
+    int DESIRED_TAG_ID = location;     // Choose the tag you want to approach or set to -1 for ANY tag.
+    AprilTagProcessor aprilTag;
+    VisionPortal visionPortal;
+    AprilTagDetection desiredTag = null;     // Used to hold the data for a detected AprilTag
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
@@ -50,6 +75,18 @@ public class AutoMeet3 extends LinearOpMode {
         huskyLens = hardwareMap.get(HuskyLens.class, "huskylens");
         Pose2d startPose = new Pose2d(0, 0, 180);
         drive.setPoseEstimate(startPose);
+
+        boolean targetFound     = false;    // Set to true when an AprilTag target is detected
+        double  move           = 1;        // Desired forward power/speed (-1 to +1)
+        double  strafe          = 1;        // Desired strafe power/speed (-1 to +1)
+        double  turn            = 1;        // Desired turning power/speed (-1 to +1)
+
+        // Initialize the Apriltag Detection process
+        initAprilTag();
+
+        setManualExposure(6, 250);
+
+
 
         //Middle Movement
         TrajectorySequence startM = drive.trajectorySequenceBuilder(startPose)
@@ -166,9 +203,6 @@ public class AutoMeet3 extends LinearOpMode {
          * within the OpMode by calling selectAlgorithm() and passing it one of the values
          * found in the enumeration HuskyLens.Algorithm.
          */
-        huskyLens.selectAlgorithm(HuskyLens.Algorithm.COLOR_RECOGNITION);
-
-
         waitForStart();
         servoLauncher.setPosition(0.2);
         motorSlideLeft.setTargetPosition(0);
@@ -181,6 +215,10 @@ public class AutoMeet3 extends LinearOpMode {
         long end = start + 2000;
         huskyLens.selectAlgorithm(HuskyLens.Algorithm.COLOR_RECOGNITION);
         while (opModeIsActive()) {
+
+            targetFound = false;
+            desiredTag  = null;
+
             if (!rateLimit.hasExpired()) {
                 continue;
             }
@@ -212,16 +250,14 @@ public class AutoMeet3 extends LinearOpMode {
                 break;
             }
         }
-
-
-            if (location == 2) {
+        if (location == 2) {
                 drive.followTrajectorySequence(startM);
                 motorIntake.setPower(-0.45);
                 drive.followTrajectorySequence(backM);
                 motorIntake.setPower(0);
                 drive.followTrajectorySequence(moveM);
 
-                //END PART DO NOT CHANGE
+                //Viper Slide Up
                 motorSlideRight.setTargetPosition(1000);
                 motorSlideLeft.setTargetPosition(1000);
                 motorSlideRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -239,10 +275,41 @@ public class AutoMeet3 extends LinearOpMode {
                 motorSlideRight.setVelocity(1000);
                 motorSlideLeft.setVelocity(1000);
                 sleep(3400);
+
+                //Move to Board & Drop
                 drive.followTrajectorySequence(leftM);
-                sleep(300);
+
+                // Step through the list of detected tags and look for a matching tag
+                List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+                for (AprilTagDetection detection : currentDetections) {
+                    // Look to see if we have size info on this tag.
+                    if (detection.metadata != null) {
+                        //  Check to see if we want to track towards this tag.
+                        if (detection.id == DESIRED_TAG_ID) {
+                            // Yes, we want to use this tag.
+                            targetFound = true;
+                            desiredTag = detection;
+                        }
+                    } else {
+                        //add code to run override trajectory sequence
+                    }
+                }
+
+                double  rangeError      = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
+                double  headingError    = desiredTag.ftcPose.bearing;
+                double  yawError        = desiredTag.ftcPose.yaw;
+
+                // Use the speed and turn "gains" to calculate how we want the robot to move.
+                move  = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+                turn   = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN) ;
+                strafe = Range.clip(-yawError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
+                telemetry.addData("Auto","Drive %5.2f, Strafe %5.2f, Turn %5.2f ", move, strafe, turn);
+                moveRobot(move, strafe, turn);
+                sleep(100);
                 servoFOT.setPosition(0.72);
                 sleep(200);
+
+                //Move Away and Viper Slide Down
                 drive.followTrajectorySequence(backwardM);
                 motorSlideRight.setTargetPosition(1000);
                 motorSlideLeft.setTargetPosition(1000);
@@ -361,5 +428,99 @@ public class AutoMeet3 extends LinearOpMode {
             }
         }
 
+    /**
+     * Move robot according to desired axes motions
+     * <p>
+     * Positive X is forward
+     * <p>
+     * Positive Y is strafe left
+     * <p>
+     * Positive Yaw is counter-clockwise
+     */
+    public void moveRobot(double x, double y, double yaw) {
+        // Calculate wheel powers.
+        double leftFrontPower    =  x -y -yaw;
+        double rightFrontPower   =  x +y +yaw;
+        double leftBackPower     =  x +y -yaw;
+        double rightBackPower    =  x -y +yaw;
+
+        // Normalize wheel powers to be less than 1.0
+        double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower /= max;
+            rightFrontPower /= max;
+            leftBackPower /= max;
+            rightBackPower /= max;
+        }
+
+        // Send powers to the wheels.
+        leftFrontDrive.setPower(leftFrontPower);
+        rightFrontDrive.setPower(rightFrontPower);
+        leftBackDrive.setPower(leftBackPower);
+        rightBackDrive.setPower(rightBackPower);
     }
+
+    /**
+     * Initialize the AprilTag processor.
+     */
+    private void initAprilTag() {
+        // Create the AprilTag processor by using a builder.
+        aprilTag = new AprilTagProcessor.Builder().build();
+
+        // Adjust Image Decimation to trade-off detection-range for detection-rate.
+        // eg: Some typical detection data using a Logitech C920 WebCam
+        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
+        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
+        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second
+        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second
+        // Note: Decimation can be changed on-the-fly to adapt during a match.
+        aprilTag.setDecimation(2);
+
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .addProcessor(aprilTag)
+                .build();
+    }
+
+    /*
+     Manually set the camera gain and exposure.
+     This can only be called AFTER calling initAprilTag(), and only works for Webcams;
+    */
+    private void    setManualExposure(int exposureMS, int gain) {
+        // Wait for the camera to be open, then use the controls
+
+        if (visionPortal == null) {
+            return;
+        }
+
+        // Make sure camera is streaming before we try to set the exposure controls
+        if (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
+            telemetry.addData("Camera", "Waiting");
+            telemetry.update();
+            while (!isStopRequested() && (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
+                sleep(20);
+            }
+            telemetry.addData("Camera", "Ready");
+            telemetry.update();
+        }
+
+        // Set camera controls unless we are stopping.
+        if (!isStopRequested())
+        {
+            ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
+            if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
+                exposureControl.setMode(ExposureControl.Mode.Manual);
+                sleep(50);
+            }
+            exposureControl.setExposure((long)exposureMS, TimeUnit.MILLISECONDS);
+            sleep(20);
+            GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
+            gainControl.setGain(gain);
+            sleep(20);
+        }
+    }
+}
 
